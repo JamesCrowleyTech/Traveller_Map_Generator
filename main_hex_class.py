de3ebbabb05ba51dc976f9e22f44341a39b2b6c4
JamesCrowleyTech <76074300+JamesCrowleyTech@users.noreply.github.com>
2021-01-18 11:48:09 +0000

    Update main_hex_class.py

--- main_hex_class.py
+++ main_hex_class.py
@@ -5,7 +5,9 @@ from math import sqrt
 
 class Hex:
     def __init__(self, true_width, true_height, mid_width, mid_height, subsector_width,
-                 subsector_height, top, left, hex_edge_length, index, display, random=True, starport=None, gas_giant=None):
+                 subsector_height, top, left, hex_edge_length, index, display, random=True, starport=None,
+                 gas_giant=None, name=None, valid=None):
+
         self.is_random = random
         self.left = left
         self.top = top
@@ -17,16 +19,19 @@ class Hex:
         self.subsector_height = subsector_height
         self.subsector_width = subsector_width
         self.index = index
-        self.is_valid = None
+        self.is_valid = valid
         self.gas_giant = None
         self.starport = None
-        self.name = None
         self.button_parameters = None
         self.display = display
         self.apothem = sqrt(3) / 2
         self.dot_width = self.hex_edge_length // 6
         self.white = (255, 255, 255)
         self.b = (0, 0, 0)
+        self.name = None
+        self.starport = starport
+        self.name = name
+        self.gas_giant = gas_giant
         self.make()
 
     def make(self):
@@ -69,19 +74,20 @@ class Hex:
             yield "".join([choice(capital_syllables)] + [choice(mid_syllables) if choice((True, False)) else ""]
                           + [choice(end_syllables)])
 
-        gas_giant_generator_object = gas_giant_generator()
-        is_valid_generator_object = is_valid_generator()
-        starport_generator_object = starport_generator()
-        name_generator_object = name_generator()
+        if self.is_random:
+            gas_giant_generator_object = gas_giant_generator()
+            is_valid_generator_object = is_valid_generator()
+            starport_generator_object = starport_generator()
+            name_generator_object = name_generator()
 
-        self.is_valid = next(is_valid_generator_object)
-        tmp_name = next(name_generator_object)
-        tmp_starport = next(starport_generator_object)
-        tmp_gas_giant = next(gas_giant_generator_object)
-        if self.is_valid:
-            self.name = tmp_name
-            self.starport = tmp_starport
-            self.gas_giant = tmp_gas_giant
+            self.is_valid = next(is_valid_generator_object)
+            tmp_name = next(name_generator_object)
+            tmp_starport = next(starport_generator_object)
+            tmp_gas_giant = next(gas_giant_generator_object)
+            if self.is_valid:
+                self.name = tmp_name
+                self.starport = tmp_starport
+                self.gas_giant = tmp_gas_giant
 
     def draw(self, x, y, colour=(255, 255, 255,), index_only=False):
         def main_hex_outline(p1, p2, p3, p4, p5, p6):
@@ -92,7 +98,8 @@ class Hex:
 
         def display_name():
             font = pygame.font.SysFont('arial', int(self.hex_edge_length / 4))
-            whitespaces = font.render("".join([' ' for _ in range(self.hex_edge_length // 3)]), True, (255, 255, 255), (255, 255, 255))
+            whitespaces = font.render("".join([' ' for _ in range(self.hex_edge_length // 3)]), True, (255, 255, 255),
+                                      (255, 255, 255))
             white_rect = whitespaces.get_rect()
             white_rect.center = (x, y + int(self.hex_edge_length / 2))
             self.display.blit(whitespaces, white_rect)
@@ -110,19 +117,18 @@ class Hex:
             return text_rect
 
         def display_starport():
+            adj = 0 if self.is_random else 2
             font = pygame.font.SysFont('arial', int(self.hex_edge_length / 3.5))
             text = font.render(f' {self.starport} ', True, self.b, colour)
             text_rect = text.get_rect()
-            text_rect.center = (x, y - int(self.hex_edge_length / 3))
+            text_rect.center = (x - adj, y - int(self.hex_edge_length / 3))
             self.display.blit(text, text_rect)
 
         def display_gas_giant():
-            black = (0, 0, 0)
-            white = (255, 255, 255)
-            colour = black
-            if not self.gas_giant: colour = white
-            pygame.draw.circle(self.display, colour, (x + self.hex_edge_length / 3 * 2, y - self.hex_edge_length / 5),
-                               self.hex_edge_length / 10)
+            if self.gas_giant:
+                pygame.draw.circle(self.display, (0, 0, 0), (x + self.hex_edge_length / 3 * 2, y - self.hex_edge_length / 5), self.hex_edge_length / 10)
+            else:
+                pygame.draw.circle(self.display, (255, 255, 255), (x + self.hex_edge_length / 3 * 2, y - self.hex_edge_length / 5), self.hex_edge_length / 10)
             pygame.display.update()
 
         def get_hex_parameters():
@@ -137,16 +143,15 @@ class Hex:
         def get_button_parameters(text_rectangle):
             return tuple([text_rectangle.left, text_rectangle.right, text_rectangle.top, text_rectangle.bottom])
 
-        if self.is_random:
-            if index_only:
-                display_index()
-                return
-
-            main_hex_outline(*get_hex_parameters())
-            text_rect = display_index()
-            if not self.is_valid: return get_button_parameters(text_rect)
+        if index_only and self.is_random:
+            display_index()
+            return
+        main_hex_outline(*get_hex_parameters())
+        text_rect = display_index()
+        if self.is_valid:
             star()
             display_name()
             display_starport()
             display_gas_giant()
+        return get_button_parameters(text_rect)
             return get_button_parameters(text_rect)
